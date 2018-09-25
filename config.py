bdbfbbd13c94d6dcad0eec6b65f3d85e66070fa4
larumuga <larumuga@scspc709.cs.uwaterloo.ca>
2018-09-25 19:37:19 -0400

    send mail test script added

--- config.py
+++ config.py
@@ -10,3 +10,10 @@ class Config(object):
     #mysql+mysqldb://<user>:<password>@<host>[:<port>]/<dbname>
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    # MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    # MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    # MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or 'Test Scrawler'
+    # MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    # ADMINS = ['scrawler16.1@gmail.com']
