4ea7c9c0c56e9933279ed48feb453eaf954d71b7
Laksh <lakshmanansathya@gmail.com>
2019-02-10 20:30:56 -0500

    minor UI update

--- app/routes.py
+++ app/routes.py
@@ -58,7 +58,7 @@ def tool_upload(token):
   form.papername.data = payload['papername']
 
   if form.validate_on_submit():
-    paper = Paper(paper_name=form.papername.data, author_name=form.authorname.data, author_email=form.authoremail.data, tool_name=form.toolname.data, link_to_pdf=form.linktopdf.data, link_to_archive=form.linktoarchive.data, link_to_tool_webpage=form.linktotoolwebpage.data, link_to_demo=form.linktodemo.data, bibtex=form.bibtex.data, description=form.description.data)
+    paper = Paper(paper_name=form.papername.data, author_name=form.authorname.data, author_email=form.authoremail.data, tool_name=form.toolname.data, link_to_pdf=form.linktopdf.data, link_to_archive=form.linktoarchive.data, link_to_tool_webpage=form.linktotoolwebpage.data, link_to_demo=form.linktodemo.data, bibtex=form.bibtex.data, description=form.description.data, view_count=1)
 
     # print(form.tags.data)
 
