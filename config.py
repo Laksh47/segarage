f688871b7c404299edec904bcf2d80b5309ebfbc
Vikram N. Subramanian <vikram.subramanian6@gmail.com>
2019-06-17 19:29:39 -0400

    trash commits

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
   SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-    'mysql://root:@localhost/segarage'
+    'mysql://root:hello123@localhost/segarage'
 
   SQLALCHEMY_TRACK_MODIFICATIONS = False
 
